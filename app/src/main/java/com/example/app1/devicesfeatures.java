147a5979ee90aa8729f2b329e18469a071c26060
liyao-l-y <2034433477@qq.com>
2025-01-07 16:40:55 +0800

    2025.1.7-8
    features

--- app/src/main/java/com/example/app1/devicesfeatures.java
+++ app/src/main/java/com/example/app1/devicesfeatures.java
@@ -1,4 +1,34 @@
 package com.example.app1;
 
+import android.content.Context;
+import android.content.pm.FeatureInfo;
+import android.content.pm.PackageManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class devicesfeatures {
+    public String features(Context context){
+        List<String> f = checkDeviceFeatures(context);
+        StringBuilder fr = new StringBuilder("支持软硬件：\n");
+        for (String feature : f){
+            fr.append(feature).append("\n");
+        }
+
+        return fr.toString();
+    }
+
+    public List<String> checkDeviceFeatures(Context context) {
+        List<String> f = new ArrayList<>();
+        PackageManager pm = context.getPackageManager();
+        FeatureInfo[] features = pm.getSystemAvailableFeatures();
+        for (FeatureInfo feature : features) {
+            // 检查特征名称是否不为null，并且是软件特征
+            if (feature.name != null) {
+                f.add(feature.name); // 输出特征名称
+            }
+        }
+        return f;
+    }
+
 }
